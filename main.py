dfbeea2428c179681d27bd72d505f8c39ec16d0d
alphapara97 <asasmal@syr.edu>
2023-05-31 16:36:28 -0400

    data validation stage 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummerizer.logging import logger
 from textSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +13,13 @@ except Exception as e:
     logger.exception(e)
     raise e
     
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} <<<<<<<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<\n\n xxxx============xxxx")
+
+except Exception as e:
+    logger.exception(e)
+    raise e    
